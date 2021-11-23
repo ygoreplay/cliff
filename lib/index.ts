4a9821e7dd9b68190028a36032fe63975aef2c9e
Sophia <beyondthesophia@gmail.com>
2021-11-23 10:28:21 +0900

    refactor(type): add rich typings

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,10 @@
+import type { ReplayParseResult } from "./types";
+
 const cliff = require("bindings")("cliff");
 
 export function initialize(databasePath: string, scriptsDirectoryPath: string) {
     cliff.initialize(databasePath, scriptsDirectoryPath);
 }
-
-export function parse(replayFilePath: string) {
+export function parse(replayFilePath: string): ReplayParseResult {
     return cliff.parse(replayFilePath);
 }
